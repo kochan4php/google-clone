4371f8735d2b74090d22c8aac49d245aac713e4e
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-09-11 05:32:56 +0700

    hehe

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,11 @@ const Home = () => {
       <div className="flex flex-col justify-center items-center gap-10 w-full px-4">
         <h1 className="text-8xl font-bold">
           <span className="text-sky-600 dark:text-sky-500">G</span>
-          <span className="text-red-600 dark:text-red-500">o</span>
-          <span className="text-yellow-600 dark:text-yellow-500">o</span>
+          <span className="text-red-500 dark:text-red-500">o</span>
+          <span className="text-yellow-500 dark:text-yellow-500">o</span>
           <span className="text-sky-600 dark:text-sky-500">g</span>
-          <span className="text-green-600 dark:text-green-500">l</span>
-          <span className="text-red-600 dark:text-red-500">e</span>
+          <span className="text-green-500 dark:text-green-500">l</span>
+          <span className="text-red-500 dark:text-red-500">e</span>
         </h1>
 
         <input
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 const googleTheme = atom({
   key: "23456789o9pkdmaoidjiam",
   default: "dark",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export { googleTheme };
